df149433a5da595aa64cda62b0c028dfc0b60013
lukasz <lukasz.skup1999@gmail.com>
2020-12-21 19:20:15 +0100

    did some work on homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
 import * as React from "react"
-import Button from '@material-ui/core/Button';
 
 import '../css/navbar.scss';
 
+import MenuIcon from '@material-ui/icons/Menu';
+
 const Navbar = () => {
     return <div className={"navbar"}>
-        Navbar test
+        <svg xmlns="http://www.w3.org/2000/svg" width="167" height="24" viewBox="0 0 167 24" className={"logo"}>
+            <g id="Group_5" data-name="Group 5" transform="translate(-6 -16)">
+                <text id="ZŁOMRAJ" transform="translate(63 18)" fill="#dcdcdc" font-size="18" font-family="LibreBaskerville-Regular, Libre Baskerville"><tspan x="0" y="17">ZŁOMRAJ</tspan></text>
+                <rect id="Rectangle_1" data-name="Rectangle 1" width="48" height="24" transform="translate(6 16)" fill="#dcdcdc"/>
+                <text id="PHU" transform="translate(8 18)" fill="#242424" font-size="18" font-family="LibreBaskerville-Regular, Libre Baskerville"><tspan x="0" y="17">PHU</tspan></text>
+                <line id="Line_1" data-name="Line 1" x2="78" transform="translate(70.5 39.5)" fill="none" stroke="#707070" stroke-width="0.5"/>
+            </g>
+        </svg>
+        <MenuIcon className={"menu-icon"}/>
     </div>
 }
 
 export default Navbar
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 import * as React from "react"
-import Navbar from "../components/Navbar";
 import Layout from "../components/Layout";
+
+import '../css/home.scss';
 
 const IndexPage = () => {
   return <Layout>
-    siema
+    <div className={"about-section"}>
+      <h2>O FIRMIE</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      </p>
+    </div>
   </Layout>
 }
 
