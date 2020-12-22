f0f8454d96f8e16667cc61c9c7e8278a2bcd0ad9
lukasz <lukasz.skup1999@gmail.com>
2020-12-22 02:36:12 +0100

    did some work on homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,13 @@ import * as React from "react"
 import '../css/navbar.scss';
 
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import { Link } from "gatsby"
 
 const Navbar = () => {
+
+    const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+
     return <div className={"navbar"}>
         <svg xmlns="http://www.w3.org/2000/svg" width="167" height="24" viewBox="0 0 167 24" className={"logo"}>
             <g id="Group_5" data-name="Group 5" transform="translate(-6 -16)">
@@ -14,7 +19,16 @@ const Navbar = () => {
                 <line id="Line_1" data-name="Line 1" x2="78" transform="translate(70.5 39.5)" fill="none" stroke="#707070" stroke-width="0.5"/>
             </g>
         </svg>
-        <MenuIcon className={"menu-icon"}/>
+        {isDrawerOpen ? <CloseIcon className={"menu-icon"} onClick={() => {setIsDrawerOpen(false)}}/>  : <MenuIcon className={"menu-icon"} onClick={() => {setIsDrawerOpen(true)}}/>}
+        <div className={"drawer"} hidden={!isDrawerOpen}>
+            <Link to="/">Strona główna</Link>
+            <Link to="/">Cennik</Link>
+            <Link to="/">Ogłoszenia</Link>
+            <Link to="/">Galeria</Link>
+            <Link to="/">O nas</Link>
+            <Link to="/">Do pobrania</Link>
+            <Link to="/">Kontakt</Link>
+        </div>
     </div>
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import Layout from "../components/Layout";
 
 import '../css/home.scss';
+import {Button, Grid} from "@material-ui/core";
 
 const IndexPage = () => {
   return <Layout>
@@ -14,6 +15,19 @@ const IndexPage = () => {
         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
       </p>
+    </div>
+    <div className={"content-switcher"}>
+      <Grid container spacing={3}>
+        <Grid item xs={4}>
+          <Button className={"button active"}>CENNIK</Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Button className={"button"}>DOJAZD</Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Button className={"button"}>OGŁOSZENIA</Button>
+        </Grid>
+      </Grid>
     </div>
   </Layout>
 }
